d0b8ac2949ca025888bafee64e0556f74b67d122
jalquisola <joseph.alquisola@gmail.com>
2016-11-09 00:02:58 +0800

    add rest-client gem

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,10 @@ module LastFmApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    %w(services).each do |path|
+      config.autoload_paths << Rails.root.join('app', path)
+    end
+
   end
 end
