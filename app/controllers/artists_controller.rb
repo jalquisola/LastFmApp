4cbb3b0c8e5c518bef8750f932c49d4c8be16ad1
jalquisola <joseph.alquisola@gmail.com>
2016-11-10 14:45:42 +0800

    simplify artists controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -19,17 +19,18 @@ class ArtistsController < ApplicationController
   def tracks
     service = LastFmService.new
 
-    @api_response = service.get_top_tracks_by_artist(params[:name],
-                                                     params[:mbid],
-                                                     @page,
-                                                     @limit)
-    if @api_response.keys.include?(:error)
+    api_response = service.get_top_tracks_by_artist(params[:name],
+                                                    params[:mbid],
+                                                    @page,
+                                                    @limit)
+    if api_response.has_error?
       redirect_to root_url, alert: @api_response[:error]
     else
-      @tracks = @api_response['toptracks']['track']
-      @total_count = @api_response['toptracks']['@attr']['total'].to_i
-      @paginatable_array = Kaminari.paginate_array(@tracks, total_count: @total_count).page(@page.to_i).per(@limit.to_i)
-      puts @paginatable_array.inspect
+      @tracks = api_response.tracks
+      @paginatable_array = Kaminari.paginate_array(@tracks,
+                                                  total_count: api_response.total_count).
+                                                  page(@page).
+                                                  per(@limit)
     end
   end
 
